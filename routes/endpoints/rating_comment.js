a7a8c90d7a880ab31740e241ce436308c375ce7a
Marcin Łoch <marcin.loch@microsoft.wsei.edu.pl>
2021-06-11 00:22:19 +0200

    comment get

--- routes/endpoints/rating_comment.js
+++ routes/endpoints/rating_comment.js
@@ -53,12 +53,12 @@ router.get("/getAll/:id", async (req, res) => {
     console.log(req.params)
 
     try {
-        const result = await db.query("SELECT * FROM rating_comment WHERE id_rest=$1", )
+        const result = await db.query("SELECT * FROM rating_comment WHERE id_rest=$1 ORDER BY date_comment DESC", )
         console.log(result.rows)
         res.status(200).json({
             status: "success",
             data: {
-                comments: result.rows
+                comment: result.rows
             }
         })
     } catch (err) {
@@ -75,8 +75,8 @@ router.post("/add/:id", async (req, res) => {
         if (bad_words(req.body.comment))
             res.send("nie używaj brzydkich słów :(")
         else {
-            const result = await db.query("INSERT INTO rating_comment (id_rest, rating, comment, id_user) VALUES ($1, $2, $3, $4) returning *", [
-                req.params.id, req.body.rating, req.body.comment, 1
+            const result = await db.query("INSERT INTO rating_comment (id_rest, rating, comment, id_user, date_comment) VALUES ($1, $2, $3, $4, $5) returning *", [
+                req.params.id, req.body.rating, req.body.comment, 1, new Date().toJSON()
                 // req.user.id
             ])
             console.log(result.rows[0])
