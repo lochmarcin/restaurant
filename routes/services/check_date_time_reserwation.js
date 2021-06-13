e1f0f5a4519c5e854db05fb981117c3712cdef24
Marcin Łoch <46865763+marcinloch@users.noreply.github.com>
2021-06-14 00:18:51 +0200

    opentime

--- routes/services/check_date_time_reserwation.js
+++ routes/services/check_date_time_reserwation.js
@@ -3,25 +3,32 @@ const db = require("../../db")
 const moment = require('moment'); // require
 moment().format();
 
-const check_date_time_reserwation = (date_booking, time_booking) => {
-    console.log("time_booking: " + time_booking + " moment: " + moment().local().format("HH-mm"))
-    console.log("date_booking: " + date_booking + " moment: " + moment().local().format("YYYY-MM-D"))
+const check_date_time_reserwation = async (date_booking, time_booking) => {
+    try {
 
-    let correct = true
-    if (date_booking > moment().local().format("YYYY-MM-D")) {
-        console.log("data większa :)")
-    }
-    if (date_booking == "2021-06-30") {
-        console.log("data == 2021-06-30")
-    }
-    else {
-        console.log("chuja")
-    }
-    if (time_booking > moment().local().format("HH-mm")) {
-        console.log("godzina większa :)")
-    }
-    else {
-        console.log("chuja")
+        console.log("time_booking: " + time_booking + " moment: " + moment().local().format("HH-mm"))
+        console.log("date_booking: " + date_booking + " moment: " + moment().local().format("YYYY-MM-D"))
+
+        let correct = true
+        if (date_booking > moment().local().format("YYYY-MM-D")) {
+            console.log("data większa :)")
+        }
+        const response = await db.query("SELECT date_booking FROM reserwation WHERE id_user=1")
+        console.log(response.rows[0].date_booking)
+        if (date_booking == response.rows[0].date_booking) {
+            console.log("to samo z bazy kurwa :D")
+        }
+        else {
+            console.log("chuja")
+        }
+        if (time_booking > moment().local().format("HH-mm")) {
+            console.log("godzina większa :)")
+        }
+        else {
+            console.log("chuja")
+        }
+    } catch (err) {
+        console.log(err)
     }
 
 
