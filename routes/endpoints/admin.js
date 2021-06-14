ee992ae66533fbc199deec4332e19014abb8d50a
Marcin Łoch <marcin.loch@microsoft.wsei.edu.pl>
2021-06-14 23:26:46 +0200

    admin

--- routes/endpoints/admin.js
+++ routes/endpoints/admin.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require("../../db")
 
 
-// router.get("/getAllRestaurant", async(req,res)=>{
+// router.get("/getAllUsers", async(req,res)=>{
 //     const result = await db.query("SELECT user.id, users.name, restaurant.name FROM users INNER JOIN restaurant ON users.id = restaurant.user_id")
 //     console.log(result.rows)
 //     res.status(200).json({
