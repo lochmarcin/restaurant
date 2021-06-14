c27ecbd7bb7f80b5f44c1c22d10622e1b50a6e2b
Marcin Łoch <marcin.loch@microsoft.wsei.edu.pl>
2021-06-14 23:51:34 +0200

    admin

--- routes/endpoints/admin.js
+++ routes/endpoints/admin.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 const db = require("../../db")
 
+router.get("/getUsers", async(req,res)=>{
+    try {
+        const result = await db.query("SELECT users.id, users.name, restaurant.name FROM users INNER JOIN restaurant ON users.id != restaurant.user_id")
+        console.log(result.rows)
+        res.status(200).json({
+            data: result.rows
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 router.get("/getAllUsers", async(req,res)=>{
     try {
@@ -18,7 +30,8 @@ router.get("/getAllUsers", async(req,res)=>{
 
 router.get("/getRestaurant/:id", async(req,res)=>{
     try {
-        const param = req.body.params
+        console.log(req.params)
+        const param = req.params
         const result = await db.query("SELECT users.id, users.name AS user_name, restaurant.name AS res_name, restaurant.category, restaurant.phone, restaurant.city, restaurant.street, restaurant.apart_number FROM users INNER JOIN restaurant ON users.id = restaurant.user_id WHERE users.id = $1",[param])
         console.log(result.rows[0])
         res.status(200).json({
